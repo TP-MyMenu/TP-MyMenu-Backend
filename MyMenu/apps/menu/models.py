59600cc986fba1ae41e2e3575d25e35c2ab84741
Facundo Gerstner <fgerstner@devartis.com>
2019-09-28 16:50:08 -0300

    add dish model

--- MyMenu/apps/menu/models.py
+++ MyMenu/apps/menu/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(_('name'), max_length=30)
+    description = models.CharField(_('description'), max_length=300)
+    image = models.ImageField(_('image'), upload_to='dishes/', null=True, blank=True)
+    price = models.FloatField(_('price'), default=0)
