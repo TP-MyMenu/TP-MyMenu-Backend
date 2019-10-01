bf08049ee0b80c1f68cacd9514646d8deb9464a8
Facu G <fgerstner@devartis.com>
2019-10-01 14:20:42 -0300

    add permissions

--- MyMenu/apps/menu/views.py
+++ MyMenu/apps/menu/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from MyMenu.apps.menu.models import Dish
 from MyMenu.apps.menu.serializers import DishSerializer
@@ -7,3 +8,4 @@ from MyMenu.apps.menu.serializers import DishSerializer
 class DishViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    permission_classes = [IsAuthenticated, ]
