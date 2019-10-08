4d3cf77afa4d8a92f2e435ed6e60a332bdae8040
Facundo Gerstner <fgerstner@devartis.com>
2019-10-08 18:54:25 -0300

    lint fixes

--- MyMenu/apps/menu/models.py
+++ MyMenu/apps/menu/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from django.core.files.storage import FileSystemStorage
+
+
 class OverwriteStorage(FileSystemStorage):
     def _save(self, name, content):
         if self.exists(name):
@@ -11,10 +13,11 @@ class OverwriteStorage(FileSystemStorage):
     def get_available_name(self, name,**kwargs):
         return name
 
+
 class Dish(models.Model):
     name = models.CharField(_('name'), max_length=30)
     description = models.CharField(_('description'), max_length=300)
-    image = models.ImageField(_('image'), upload_to='dishes/', null=True, blank=True,storage=OverwriteStorage())
+    image = models.ImageField(_('image'), upload_to='dishes/', null=True, blank=True, storage=OverwriteStorage())
     price = models.FloatField(_('price'), default=0)
     
     class Meta:
