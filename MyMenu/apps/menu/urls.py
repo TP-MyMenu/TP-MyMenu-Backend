d15ed9e61be923e67d867328426f0517e1ca8fc3
Facundo Gerstner <facugerstner_29@hotmail.com>
2019-11-09 16:37:13 -0300

    Update urls.py

--- MyMenu/apps/menu/urls.py
+++ MyMenu/apps/menu/urls.py
@@ -6,6 +6,10 @@ from MyMenu.apps.menu import views
 router = routers.DefaultRouter()
 router.register(r'dishes', views.DishViewSet)
 router.register(r'drinks', views.DrinkViewSet)
+router.register(r'day_dish', views.DayDishViewSet)
+router.register(r'suggestion', views.SuggestionViewSet)
+router.register(r'payment_method', views.PaymentMethodViewSet)
+router.register(r'dish_category', views.DishCategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
